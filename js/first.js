52da5cf9317e34e767fdd4b420d725b97767cb09
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-05-30 09:59:49 +0530

    test to fix bugs

--- js/first.js
+++ js/first.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     $('#first_page').mousemove(function(a) {
-        var mousex = (a.pageX * -1 / 10);
-        var mousey = (a.pageY * -1 / 10);
+        var mousex = (a.pageX * -0.005);
+        var mousey = (a.pageY * -0.005);
         $('#first_page_back_img ').css('background-position', mousex + 'px ' + mousey + 'px');
     });
     $('#first_page_writing_contents').mousemove(function(a) {
-        var mousex = (a.pageX * -1 / 10);
-        var mousey = (a.pageY * -1 / 10);
+        var mousex = (a.pageX * -0.005);
+        var mousey = (a.pageY * -0.005);
         $('#first_page_back_img ').css('background-position', mousex + 'px ' + mousey + 'px');
     });
 });
