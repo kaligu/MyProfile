ab5e98b574236afcdf0df709a0fc6d43db425611
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-06-21 10:50:39 +0530

    finalizing first page

--- js/first.js
+++ js/first.js
@@ -49,14 +49,14 @@ $(document).ready(function() {
     var index = 0;
 
     setInterval(function() {
-        $('.fp-sen_3 > p:nth-child(1)').text(texts[index]);
+        $('.fp-con-cnt-sents > p:nth-child(4)').text(texts[index]);
         index = (index + 1) % texts.length;
     }, 2000);
 });
 
 $(document).ready(function() {
     setInterval(function() {
-        $('.fp-avatar').toggleClass('flipped');
+        $('.fp-con-cnt-avtr').toggleClass('flipped');
     }, 8000);
 });
 
@@ -76,4 +76,46 @@ function flipCard() {
 setInterval(flipCard, 7500); // 180000 milliseconds = 3 minutes
 
 
+document.addEventListener('DOMContentLoaded',function(event){
+    // array with texts to type in typewriter
+    var dataText = ["Chethiya", "Kaligu"];
+
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+        // chekc if text isn't finished yet
+        if (i < (text.length)) {
+            // add next character to h1
+            document.querySelector(".fp-con-cnt-sents > p:nth-child(2)").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+            // wait for a while and call this function again for next character
+            setTimeout(function() {
+                typeWriter(text, i + 1, fnCallback)
+            }, 100);
+        }
+        // text finished, call callback if there is a callback function
+        else if (typeof fnCallback == 'function') {
+            // call callback after timeout
+            setTimeout(fnCallback, 700);
+        }
+    }
+    // start a typewriter animation for a text in the dataText array
+    function StartTextAnimation(i) {
+        if (typeof dataText[i] == 'undefined'){
+            setTimeout(function() {
+                StartTextAnimation(0);
+            }, 5000);
+        }
+        // check if dataText[i] exists
+        if (i < dataText[i].length) {
+            // text exists! start typewriter animation
+            typeWriter(dataText[i], 0, function(){
+                // after callback (and whole text has been animated), start next text
+                StartTextAnimation(i + 1);
+            });
+        }
+    }
+    // start the text animation
+    StartTextAnimation(0);
+});
 
